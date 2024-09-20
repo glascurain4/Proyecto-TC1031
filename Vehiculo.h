58adf1bf8f6281b56500a31f43a27760f7633fad
glascurain4 <142357491+glascurain4@users.noreply.github.com>
2024-09-20 10:54:06 -0600

    Add files via upload

---
+++ Vehiculo.h
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vehiculo {
+  private:
+    float precio;
+    string color;
+    string tipo;
+    string marca;
+    int anio;
+
+  public:
+    Vehiculo();
+    Vehiculo(float, string, string, string, int);
+    float getPrecio();
+    string getMarca();
+    int getAnio();
+    string getDatos();
+};
+
+Vehiculo::Vehiculo() {
+  precio = 0;
+  color = "";
+  tipo = "";
+  marca = "";
+  anio = 0;
+}
+
+Vehiculo::Vehiculo(float pre, string col, string tip, string mar, int ani) {
+  precio = pre;
+  color = col;
+  tipo = tip;
+  marca = mar;
+  anio = ani;
+}
+
+float Vehiculo::getPrecio() {
+  return precio;
+}
+
+string Vehiculo::getMarca() {
+  return marca;
+}
+
+int Vehiculo::getAnio() {
+  return anio;
+}
+
+string Vehiculo::getDatos() {
+  return  marca + " " + tipo + " " + color + " (" + to_string(anio) + ") - $" + to_string(precio);
+}
